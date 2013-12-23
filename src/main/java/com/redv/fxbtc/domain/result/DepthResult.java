467ef1c0438b49dc8c3d1f29938980664f14b299
Joe Zhou <zhoushuqun@gmail.com>
2013-12-23 19:26:03 +0800

    Fix problem found by PMD.

--- src/main/java/com/redv/fxbtc/domain/result/DepthResult.java
+++ src/main/java/com/redv/fxbtc/domain/result/DepthResult.java
@@ -3,20 +3,28 @@ package com.redv.fxbtc.domain.result;
 import com.fasterxml.jackson.annotation.JsonProperty;
 import com.redv.fxbtc.domain.Depth;
 import com.redv.fxbtc.domain.Params;
+import com.redv.fxbtc.domain.Symbol;
 
 public class DepthResult extends MarketDataResult<Depth> {
 
 	private static final long serialVersionUID = 2013122101L;
+
+	private Symbol symbol;
 
 	public DepthResult(@JsonProperty("result") boolean result,
 			@JsonProperty("depth") Depth data,
 			@JsonProperty("params") Params params,
 			@JsonProperty("symbol") String symbol) {
 		super(result, data, params);
+		this.symbol = Symbol.valueOfSymbol(symbol);
 	}
 
 	public Depth getDepth() {
 		return getData();
+	}
+
+	public Symbol getSymbol() {
+		return symbol;
 	}
 
 }

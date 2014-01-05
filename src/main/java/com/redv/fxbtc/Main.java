00e94b43f1588fefaff0bca879030a56a5f6fe96
Joe Zhou <zhoushuqun@gmail.com>
2014-01-05 16:51:52 +0800

    Comment out the bid calling from the test class Main.

--- src/main/java/com/redv/fxbtc/Main.java
+++ src/main/java/com/redv/fxbtc/Main.java
@@ -1,7 +1,6 @@
 package com.redv.fxbtc;
 
 import java.io.IOException;
-import java.math.BigDecimal;
 import java.util.List;
 
 import org.slf4j.Logger;
@@ -13,7 +12,6 @@ import com.redv.fxbtc.domain.Order;
 import com.redv.fxbtc.domain.Symbol;
 import com.redv.fxbtc.domain.Ticker;
 import com.redv.fxbtc.domain.Trade;
-import com.redv.fxbtc.domain.TradeInfo;
 
 public class Main {
 
@@ -51,8 +49,9 @@ public class Main {
 			log.debug("Order: {}", order);
 		}
 
-		TradeInfo tradeInfo = client.bid(Symbol.BTC_CNY, new BigDecimal("0.01"), new BigDecimal("0.001"));
-		System.out.println(tradeInfo);
+		// Bid
+		// TradeInfo tradeInfo = client.bid(Symbol.BTC_CNY, new BigDecimal("0.01"), new BigDecimal("0.001"));
+		// System.out.println(tradeInfo);
 	}
 
 }
